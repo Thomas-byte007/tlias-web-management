b901fd7a24d32c992b8afa01ed1667c5e2130e37
bytes <2057335724@qq.com>
2025-12-05 15:00:16 +0800

    Add function of file upload

--- src/test/java/com/itheima/TliasWebManagementApplicationTests.java
+++ src/test/java/com/itheima/TliasWebManagementApplicationTests.java
@@ -3,9 +3,19 @@ package com.itheima;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 @SpringBootTest
 class TliasWebManagementApplicationTests {
 
+    @Test
+    public void testUuid(){
+        for (int i = 0; i < 1000; i++) {
+            String uuid = UUID.randomUUID().toString();
+            System.out.println(uuid);
+        }
+    }
+    
     @Test
     void contextLoads() {
     }
